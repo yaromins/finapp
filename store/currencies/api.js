046ba0538c7489e82cf41edf9881eaf71c129d16
Sergey Eremin <seremin@gmail.com>
2021-01-18 10:19:28 +0300

    added BYN currency exchange rates download from nbrb.by

--- store/currencies/api.js
+++ store/currencies/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const serviceUrl = 'https://api.openrates.io'
+const openratesUrl = 'https://api.openrates.io'
+const nbrbUrl = 'https://www.nbrb.by'
 
 export const getRatesOf = async (baseCurrency) => {
   if (!baseCurrency) {
@@ -8,8 +9,13 @@ export const getRatesOf = async (baseCurrency) => {
     return
   }
 
-  const currencies = await axios.get(`${serviceUrl}/latest?base=${baseCurrency}`)
+  const currencies = await axios.get(`${openratesUrl}/latest?base=${baseCurrency}`)
+  const base2byn = await axios.get(`${nbrbUrl}/api/exrates/rates/${baseCurrency}?parammode=2`)
   if (currencies && currencies.data) {
+    if (base2byn) {
+      const { Cur_Scale, Cur_OfficialRate } = base2byn.data
+      currencies.data.rates["BYN"] = Cur_OfficialRate / Cur_Scale;
+    }
     return currencies.data.rates
   }
   else {
